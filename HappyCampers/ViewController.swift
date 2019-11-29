382d6c426d8f37f9bfb2d874aff7d1a6be137ef2
Nathan Broyles <nathanbroyles@gmail.com>
2019-11-29 09:45:34 -0800

    Initial Commit

---
+++ HappyCampers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyCampers
+//
+//  Created by Nathan Broyles on 11/29/19.
+//  Copyright © 2019 DeadPixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
